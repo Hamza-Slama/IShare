db72071599948cda5aa60cc0f9b810e9bce3d558
Hamza Slama <hamzaslama8@gmail.com>
2018-03-28 02:07:11 +0200

    Add test Home Activity

--- app/src/main/java/ia2/moduleproject/eniso/ishare/Constants/Constants.kt
+++ app/src/main/java/ia2/moduleproject/eniso/ishare/Constants/Constants.kt
@@ -4,9 +4,5 @@ package ia2.moduleproject.eniso.ishare.Constants
  * Created by Hamza on 23/02/2018.
  */
 
-var MyREPONSE = "MyREPONSE"
-var MyUserLoginAndPassword = "MyUserLoginAndPassword "
-fun SplitString(email:String):String{
-    val split= email.split("@")
-    return split[0]
-}
+val MyREPONSE = "MyREPONSE"
+val MyUserLoginAndPassword = "MyUserLoginAndPassword "
