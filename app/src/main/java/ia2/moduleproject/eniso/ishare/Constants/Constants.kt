ef558dbe39fc6ad8ea430bdd90efc1d80c59ab5f
Hamza Slama <hamzaslama8@gmail.com>
2018-05-26 00:28:49 +0100

    update file directory

--- app/src/main/java/ia2/moduleproject/eniso/ishare/Constants/Constants.kt
+++
@@ -1,8 +0,0 @@
-package ia2.moduleproject.eniso.ishare.Constants
-
-/**
- * Created by Hamza on 23/02/2018.
- */
-
-val MyREPONSE = "MyREPONSE"
-val MyUserLoginAndPassword = "MyUserLoginAndPassword "
--- app/src/main/java/ia2/moduleproject/eniso/ishare/Model/UserInfo.kt
+++
@@ -1,6 +0,0 @@
-package ia2.moduleproject.eniso.ishare.Model
-
-/**
- * Created by hamza on 5/9/18.
- */
-class UserInfo (var user_id : String , var first_name :String , var email : String , var picture_path : String )
